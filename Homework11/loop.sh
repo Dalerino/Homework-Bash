645999a0836930c89e9595274e6942e12ba1da25
Dalerino <baitursunov@gmail.com>
2024-05-29 21:05:13 +0000

    new adjustments in the code

--- Homework11/loop.sh
+++ Homework11/loop.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
-for user in emma mia chloe zoe
+function create_users() {
+  for i in emma mia chloe zoe
 do
-  if ! id -u $user &>/dev/null
-then
-  useradd $user
-  echo "User $user created."
-else
-  echo "User $user already exists."
-fi
+  useradd $i
 done
+}
 
-for folder in day week month year
+function create_folders() {
+  for a in day week month year
 do
-  if [ ! -d $folder ]
-then
-  mkdir $folder
-  echo "Folder $folder created."
-else
-  echo "Folder $folder already exists."
-fi
+  mkdir $a
 done
+}
 
-yum install -y wget httpd php php-mysql
-systemctl start httpd
-systemctl enable httpd
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
-rsync -avP wordpress/ /var/www/html/
-chown -R apache:apache /var/www/html/
-chmod -R 755 /var/www/html/
-systemctl restart httpd
- echo "WordPress installed."
+function install_wordpress() {
+    yum install -y httpd wget unzip epel-release mysql
+    yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm 
+    yum-config-manager --enable remi-php74 
+    yum install -y php 
+    yum install -y php-mysql 
+    wget https://wordpress.org/latest.tar.gz 
+    tar -xf latest.tar.gz -C /var/www/html/ 
+    mv /var/www/html/wordpress/* /var/www/html/ 
+    chown -R apache:apache /var/www/html/ 
+    rm -rf /var/www/html/index.html 
+    systemctl restart httpd
+    systemctl enable httpd
+}
 
-
-if [ "$1" == "create_users" ]
-then
-   create_users
-elif [ "$2" == "create_folders" ]
-then
+read -p "Enter the function name to execute (user/folder/install wordpress): " function_name
+if [[ $function_name == "create_users" ]]; then
+    create_users
+elif [[ $function_name == "create_folders" ]]; then
     create_folders
-elif [ "$3" == "install_wordpress" ]
- then
+elif [[ $function_name == "install_wordpress" ]]; then
     install_wordpress
 else
-    echo "Invalid options. Please use create_users, create_folders, or install_wordpress as arguments."
+    echo "Invalid function name. Please enter either 'user' 'folder' or 'install wordpress'."
 fi
+
 
